4064560b8ff05bcac3435ec7f382f0d02211a4d8
jkaupp <jkaupp@gmail.com>
2017-05-23 12:28:17 -0400

    Cleaned dir

--- tidy.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-library(googlesheets)
-
-framework <- gs_title("(17-05-05)Data-Needs_Shared.xlsx") %>%
-  gs_read(ws = 2)
-
-feather::write_feather(framework, "easel/framework.feather")
-
